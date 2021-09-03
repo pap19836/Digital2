a3bb65f7585db0595e5cb12bed18e1de26c6fc17
fue19389 <68131348+fue19389@users.noreply.github.com>
2021-09-03 16:04:08 -0600

    Adafruit comentado

--- Proyecto_1/ADAPROYECTO.py
+++ Proyecto_1/ADAPROYECTO.py
@@ -14,69 +14,73 @@ import serial
 import time
 
 
-ser = serial.Serial('COM2', baudrate = 9600)
+ser = serial.Serial('COM2', baudrate = 9600)  #Inicialización de serial
 
-ADAFRUIT_IO_KEY = "aio_YAsE55Qo3Ewin96ARqeYnmza0wXa"
+ADAFRUIT_IO_KEY = "aaio_YAsE55Qo3Ewin96ARqeYnmza0wXa"  #Definición de Adafruit
 ADAFRUIT_IO_USERNAME = "YRAR"
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 while(1):
+    # Reseteo de comunicación serial
     ser.close()
     ser.open()
+
     #Readval pyserial
-    data = ser.read(7)
-    data0 = str(data)
-    print(data0)
+    data = ser.read(7) # Lectura de cada uno de los caracteres enviados
+    data0 = str(data) # Conversión a string
+
+    # Creación de array de caracteres
     flags = data0.split(',')
-    lock = flags[1]
+    lock = flags[1] # Guardado de caracteres
     lock_compare = 1
     door = flags[2]
     door_compare = 1
     light = flags[3]
+
+    #Variables
     light_compare = 1
     light_state_compare = 1
-
     light_state = 0
     light_int = 1
+
+    #Print de visualización
     print(lock)
     print(door)
     print(light)
 
 #VALORES ENVIAR
 
-    #DOOR
+    #DOOR feed set and send
     door_feed = aio.feeds('door')
     if(door != door_compare):
         door_compare = door
         aio.send_data(door_feed.key, door)
 
-    #LOCK
+    #LOCK feed set and send
     lock_feed = aio.feeds('lock')
     if(lock != lock_compare):
         lock_compare = lock
         aio.send_data(lock_feed.key, lock)
 
-    #LIGHT
+    #LIGHT feed set and send
     light_feed = aio.feeds('light')
     if(light != light_compare):
         light_compare = light
         aio.send_data(light_feed.key, light)
 
+    #LIGHT control set
     light_ctrl_feed = aio.feeds('light-ctrl')
+
+    
 #VALOR A RECIBIR
 
-    #LIGHT
+    #LIGHT control receive
     if (light_state != light_state_compare):
         light_state_compare = light_state
         light_state = aio.receive(light_ctrl_feed.key)
         print(f'LIGHT: {light_state.value}')
         light_int = int(light_state.value)
         ser.write(bytes([light_int]))
-    #if light_int == 2:
-    #    ser.write('2'.encode('utf-8'))
-    #if light_int == 1:
-    #    ser.write('1'.encode('utf-8'))
-    #if light_int == 0:
-    #    ser.write('0'.encode('utf-8'))
 
-    time.sleep(3)
+
+    time.sleep(4)
