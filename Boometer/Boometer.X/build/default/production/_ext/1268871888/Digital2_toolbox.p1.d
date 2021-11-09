44888c0a76b3f02c89f9b7cd2df6b7269a36cc53
pap19836 <pap19836@uvg.edu.gt>
2021-11-09 17:53:07 -0600

    Vumeter Test

---
+++ Boometer/Boometer.X/build/default/production/_ext/1268871888/Digital2_toolbox.p1.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/1268871888/Digital2_toolbox.p1:  \
+C:/Digital2/Boometer/Boometer.X/Digital2_toolbox.c  \
+C:/Digital2/Boometer/Boometer.X/Digital2_toolbox.h 
--- Lab09_TivaWare/main.c
+++ Lab09_TivaWare/main.c
@@ -5,32 +5,43 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 
+void antirrebote(void);
+void semaforo(void);
 
+bool push0 = 0;
+bool push0_active = 0;
 
-/**
- * Hello world
- * IE-3027
- * Diego Morales
- */
 int main(void)
 {
     SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
     while (1){
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, 0x02);
-        SysCtlDelay(16000000);
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, 0X0A);
-        SysCtlDelay(16000000);
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, 0x08);
-        SysCtlDelay(16000000);
-        int i = 0;
-        for (i; i<3; i++) {
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, 0X00);
-            SysCtlDelay(8000000);
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, 0x08);
-            SysCtlDelay(8000000);
+        if(!GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4)) push0 = 1;
+
+        if(push0 == 1){
+            push0 = 0;
+            semaforo();
         }
     }
+
 }
 
+
+void semaforo(void){
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, 0x08);
+    SysCtlDelay(16000000);
+    int i = 0;
+    for (i; i<3; i++) {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, 0X00);
+        SysCtlDelay(8000000);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, 0x08);
+        SysCtlDelay(8000000);
+    }
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, 0X0A);
+    SysCtlDelay(16000000);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_3, 0x02);
+    SysCtlDelay(16000000);
+}
